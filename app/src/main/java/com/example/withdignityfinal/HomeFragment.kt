e15c12f911d46829d3477b410e45b9e1a70512f8
Filoha Mariao <mariaolotira@gmail.com.com>
2023-12-19 11:13:14 +0300

    bottom navbar

--- app/src/main/java/com/example/withdignityfinal/HomeFragment.kt
+++ app/src/main/java/com/example/withdignityfinal/HomeFragment.kt
@@ -1,13 +1,21 @@
 package com.example.withdignityfinal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.HorizontalScrollView
+import androidx.cardview.widget.CardView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
 
+    private lateinit var scrollView: HorizontalScrollView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +23,16 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+
+
+
     }
+
+    private fun findViewById(homeScrollview: Int): HorizontalScrollView {
+        TODO("Not yet implemented")
+    }
+
 
 }
