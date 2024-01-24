317ea34f00d9b323bc487584c89288c6390048de
Brokeice123 <keithmarkomen@gmail.com>
2024-01-24 21:30:47 +0300

    Formatted the bottom navigation bar

--- app/src/main/java/com/example/withdignityfinal/MainActivity.kt
+++ app/src/main/java/com/example/withdignityfinal/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.replace
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.content.res.ColorStateList
+import android.graphics.Color
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,22 +22,27 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setOnItemSelectedListener(){ menuItem ->
             when(menuItem.itemId){
                 R.id.home_bottomnav -> {
+                    setBottomNavigationItemTextColor(R.color.white)
                     replaceFragment(HomeFragment())
                     true
                 }
                 R.id.package_bottomnav -> {
+                    setBottomNavigationItemTextColor(R.color.white)
                     replaceFragment(FuneralPackageFragment())
                     true
                 }
                 R.id.findfuneral_bottomnav -> {
+                    setBottomNavigationItemTextColor(R.color.white)
                     replaceFragment(FindFuneralFragment())
                     true
                 }
                 R.id.help_bottomnav -> {
+                    setBottomNavigationItemTextColor(R.color.white)
                     replaceFragment(HelpFragment())
                     true
                 }
                 R.id.checkout_bottomnav -> {
+                    setBottomNavigationItemTextColor(R.color.white)
                     replaceFragment(CheckoutFragment())
                     true
                 }
@@ -49,5 +57,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun replaceFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
+    private fun setBottomNavigationItemTextColor(colorResId: Int) {
+        val color = ContextCompat.getColor(this, colorResId)
+        val uncheckedColor = Color.WHITE
+
+        val colorStateList = ColorStateList(
+            arrayOf(
+                intArrayOf(android.R.attr.state_checked),
+                intArrayOf(-android.R.attr.state_checked)
+            ),
+            intArrayOf(
+                color, // Color when checked
+                uncheckedColor // Color when unchecked
+            )
+        )
+
+        bottomNavigationView.itemTextColor = colorStateList
     }
 }
